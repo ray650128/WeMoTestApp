43e2d676255536386cb3ee34c2f08e3ae0197d24
Raymond Yang <ray650128@gmail.com>
2022-03-09 23:18:22 +0800

    PlaceListFragment 串接 ViewModel

---
+++ app/src/main/java/com/ray650128/wemotestapp/view/fragment/BaseFragment.kt
@@ -0,0 +1,31 @@
+package com.ray650128.wemotestapp.view.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import java.lang.reflect.ParameterizedType
+
+abstract class BaseFragment<T : ViewBinding> : Fragment() {
+
+    lateinit var binding : T
+
+    private lateinit var mContext: Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        this.mContext = context
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // 利用反射，呼叫指定 ViewBinding 中的 inflate 方法填充 View
+        val type = javaClass.genericSuperclass as ParameterizedType
+        val mClass = type.actualTypeArguments[0] as Class<T>
+        val method = mClass.getDeclaredMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java)
+        binding = method.invoke(null, layoutInflater, container, false) as T
+        return binding.root
+    }
+}
--- app/src/main/java/com/ray650128/wemotestapp/view/fragment/PlaceListFragment.kt
+++ app/src/main/java/com/ray650128/wemotestapp/view/fragment/PlaceListFragment.kt
@@ -1,60 +1,47 @@
 package com.ray650128.wemotestapp.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.ray650128.wemotestapp.R
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ray650128.wemotestapp.databinding.FragmentPlaceListBinding
+import com.ray650128.wemotestapp.view.adapter.PlaceListAdapter
+import com.ray650128.wemotestapp.viewModel.PlaceListViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PlaceListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class PlaceListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class PlaceListFragment : BaseFragment<FragmentPlaceListBinding>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var listAdapter: PlaceListAdapter = PlaceListAdapter()
+
+    private val viewModel: PlaceListViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initList()
+
+        initObserver()
+
+        binding.floatingActionButton.setOnClickListener {
+
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_place_list, container, false)
+    private fun initList() = binding.apply {
+        listPlace.apply {
+            adapter = listAdapter
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        }
+
+        listAdapter.onItemClick = {
+
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PlaceListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PlaceListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initObserver() {
+        viewModel.listData.observe(viewLifecycleOwner) {
+            listAdapter.submitList(it)
+        }
     }
 }
