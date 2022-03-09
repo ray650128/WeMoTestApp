49658092e8ea9702e545655218462c68b0cbccdb
Raymond Yang <ray650128@gmail.com>
2022-03-09 11:29:05 +0800

    init commit

---
+++ app/src/main/java/com/ray650128/wemotestapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ray650128.wemotestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ray650128.wemotestapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
