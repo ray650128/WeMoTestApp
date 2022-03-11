ad1ce937569ee8a49e6caf94aae969ae2eab3de1
Raymond Yang <ray650128@gmail.com>
2022-03-11 12:05:18 +0800

    實作地圖功能

--- app/src/main/java/com/ray650128/wemotestapp/view/fragment/MapViewFragment.kt
+++ app/src/main/java/com/ray650128/wemotestapp/view/fragment/MapViewFragment.kt
@@ -1,60 +1,124 @@
 package com.ray650128.wemotestapp.view.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.ray650128.wemotestapp.R
+import com.ray650128.wemotestapp.databinding.FragmentMapViewBinding
+import com.ray650128.wemotestapp.model.Place
+import com.ray650128.wemotestapp.viewModel.PlaceListViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MapViewFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MapViewFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class MapViewFragment : BaseFragment<FragmentMapViewBinding>(), OnMapReadyCallback {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var mMap: GoogleMap
+
+    private val viewModel: PlaceListViewModel by activityViewModels()
+
+    private val navController: NavController by lazy { findNavController() }
+
+    private val markerOptionsGreen: MarkerOptions by lazy {
+        MarkerOptions().apply {
+            icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).alpha(1f)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map_view, container, false)
+    private val markerOptionsRed: MarkerOptions by lazy {
+        MarkerOptions()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MapViewFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MapViewFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private var placeList: ArrayList<Place> = arrayListOf()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this@MapViewFragment)
+
+        initObserver()
+    }
+
+    private fun initObserver() {
+        viewModel.listData.observe(viewLifecycleOwner) { list ->
+            placeList = ArrayList(list)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+
+        mMap.isMyLocationEnabled = true
+
+        mMap.setOnMarkerClickListener { marker ->
+            val action = MapViewFragmentDirections.mapToPlaceDetail(
+                editMode = PlaceFragment.VIEW_MODE,
+                dataId = marker.tag.toString().toInt()
+            )
+            navController.navigate(action)
+            false
+        }
+
+        initLocation()
+        createLocationRequest()
+
+        showMarker()
+    }
+
+    // 初始化位置，由於已經先在onMapReady()中要求權限了，因此無需再次要求權限
+    @SuppressLint("MissingPermission")
+    private fun initLocation() {
+        val client = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        client.lastLocation.addOnCompleteListener(requireActivity()) { task ->
+            if (task.isSuccessful) {
+                val location = task.result ?: return@addOnCompleteListener
+                val latLng = LatLng(location.latitude, location.longitude)
+                val camera = CameraUpdateFactory.newLatLngZoom(latLng, mMap.maxZoomLevel)
+                mMap.animateCamera(camera)
+            }
+        }
+    }
+
+    // 設定位置要求的參數
+    @SuppressLint("RestrictedApi")
+    private fun createLocationRequest() {
+        val locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    private fun showMarker() {
+        for (item in placeList) {
+            val latLng = LatLng(item.latitude, item.longitude)
+            if (item.photos.isNullOrEmpty()) {
+                markerOptionsRed.position(latLng)
+                markerOptionsRed.title(item.title)
+                mMap.addMarker(markerOptionsRed).apply {
+                    this?.tag = item.id
+                }
+            } else {
+                markerOptionsGreen.position(latLng)
+                markerOptionsGreen.title(item.title)
+                mMap.addMarker(markerOptionsGreen).apply {
+                    this?.tag = item.id
                 }
             }
+        }
     }
 }
